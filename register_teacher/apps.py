947594926962f72d918133083d5e8d0740ebdaaa
alesantos99 <alessandrasantosds@gmail.com>
2020-03-02 16:06:13 -0300

    GETHOURS

---
+++ register_teacher/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RegisterTeacherConfig(AppConfig):
+    name = 'register_teacher'
--- studyway/urls.py
+++ studyway/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from register_teacher.views import register
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register-teacher', register),
+
 ]
